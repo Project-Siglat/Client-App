4af32ab15195c6d273ce7035bd08ebdb0ae890dc
renzaspiras <aspirasrenz@gmail.com>
2025-07-31 14:19:22 +0000

    UI refresher

--- pages/client/index.php
+++ pages/client/index.php
@@ -1,12 +1,10 @@
 <script src="./config/session.js"></script>
 
-
 <?php
 // include "./pages/client/component/topbar.php";
 include "./components/topbar.html";
 
 include "./pages/client/component/map/index.html";
 include "./pages/client/widget/frame.php";
-
 
 ?>
