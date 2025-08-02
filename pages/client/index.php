a4e4c4e297226f5436759cdecec72b8eedb47c49
renzaspiras <aspirasrenz@gmail.com>
2025-08-02 09:57:09 +0000

    update checkout

--- pages/client/index.php
+++ pages/client/index.php
@@ -1,8 +1,7 @@
 <script src="./config/session.js"></script>
 
 <?php
-// include "./pages/client/component/topbar.php";
-// include "./components/topbar.html";
+include "./components/topbar.html";
 
 include "./pages/client/component/map/index.html";
 include "./pages/client/widget/frame.php";
