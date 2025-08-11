a1b2b128fa51e1ea08e3d77d16e931d0bb04330a
renzaspiras <aspirasrenz@gmail.com>
2025-08-11 20:32:32 +0000

    re organized source code

--- page/client/index.php
+++ page/client/index.php
@@ -1,1 +1,10 @@
-client
+<script src="./config/session.js"></script>
+
+<?php
+include "./components/topbar.html";
+
+include "./page/client/component/map/index.html";
+include "./components/general-pack/frame/frame.php";
+
+
+?>
